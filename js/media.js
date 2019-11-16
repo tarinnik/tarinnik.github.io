cec16571ecd381866aaa74cb6959cb004dc87a9c
Tinnik <tpkoster@protonmail.com>
2019-11-16 12:43:23 +1000

    Added keyboard commands for services

--- js/media.js
+++ js/media.js
@@ -9,9 +9,62 @@ window.addEventListener('load', function () {
 });
 
 function m() {
-    document.getElementById("7plus").remove();
-    document.getElementById("9now").remove();
-    document.getElementById("tenplay").remove();
     document.getElementById("twitch").remove();
-    document.getElementById("youtube").remove();
 }
+
+function select(i) {
+    const services = document.getElementById("services").childElementCount;
+    if (i < services) {
+        document.getElementsByClassName("grid-item")[i].getElementsByTagName("a")[0].click();
+    }
+}
+
+const map = {};
+onkeydown = onkeyup = function(e){
+    map[e.keyCode] = e.type === 'keydown';
+
+    // Numpad 7
+    if (map[103]) {
+        select(0);
+    }
+
+    // Numpad 8
+    else if (map[104]) {
+        select(1);
+    }
+
+    //Numpad 9
+    else if (map[105]) {
+        select(2);
+    }
+
+    //Numpad 4
+    else if (map[100]) {
+        select(3);
+    }
+
+    // Numpad 5
+    else if (map[101]) {
+        select(4);
+    }
+
+    //Numpad 6
+    else if (map[102]) {
+        select(5);
+    }
+
+    // Numpad 1
+    else if (map[97]) {
+        select(6);
+    }
+
+    // Numpad 2
+    else if (map[98]) {
+        select(7);
+    }
+
+    //Numpad 3
+    else if (map[99]) {
+        select(8);
+    }
+};
