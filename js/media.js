39a2e301c43d5cbb1bb4b6930bafc66af13a28f5
Tinnik <tpkoster@protonmail.com>
2019-11-16 13:56:57 +1000

    Changed keypress detection

--- js/media.js
+++ js/media.js
@@ -12,59 +12,45 @@ function m() {
     document.getElementById("twitch").remove();
 }
 
+document.addEventListener('keydown', function (event) {
+    key(event);
+});
+
+function key(event) {
+    switch (event.key) {
+        case '1':
+            select(6);
+            break;
+        case '2':
+            select(7);
+            break;
+        case '3':
+            select(8);
+            break;
+        case '4':
+            select(3);
+            break;
+        case '5':
+            select(4);
+            break;
+        case '6':
+            select(5);
+            break;
+        case '7':
+            select(0);
+            break;
+        case '8':
+            select(1);
+            break;
+        case '9':
+            select(2);
+            break;
+    }
+}
+
 function select(i) {
     const services = document.getElementById("services").childElementCount;
     if (i < services) {
         document.getElementsByClassName("grid-item")[i].getElementsByTagName("a")[0].click();
     }
 }
-
-const map = {};
-onkeydown = onkeyup = function(e){
-    map[e.keyCode] = e.type === 'keydown';
-
-    // Numpad 7
-    if (map[103]) {
-        select(0);
-    }
-
-    // Numpad 8
-    else if (map[104]) {
-        select(1);
-    }
-
-    //Numpad 9
-    else if (map[105]) {
-        select(2);
-    }
-
-    //Numpad 4
-    else if (map[100]) {
-        select(3);
-    }
-
-    // Numpad 5
-    else if (map[101]) {
-        select(4);
-    }
-
-    //Numpad 6
-    else if (map[102]) {
-        select(5);
-    }
-
-    // Numpad 1
-    else if (map[97]) {
-        select(6);
-    }
-
-    // Numpad 2
-    else if (map[98]) {
-        select(7);
-    }
-
-    //Numpad 3
-    else if (map[99]) {
-        select(8);
-    }
-};
