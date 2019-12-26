77357c8883978ce1d966365897a736051e9e9d8c
Tinnik <tpkoster@protonmail.com>
2019-12-26 12:18:31 +1000

    Changed media selection method

--- js/media.js
+++ js/media.js
@@ -1,10 +1,14 @@
 window.addEventListener('load', function () {
     let hide = window.localStorage.getItem("hide");
-    if (hide === "" || hide === null) return;
+    if (hide === "" || hide === null) {
+        highlight(DIRECTION.none);
+        return;
+    }
     hide = hide.split(";");
     for (let i = 0; i < hide.length; i++) {
         document.getElementById(hide[i]).remove();
     }
+    highlight(DIRECTION.none);
 });
 
 let menu = false;
@@ -15,6 +19,8 @@ const DIRECTION = {
     remove: 'r',
     forward: 1,
     backwards: -1,
+    up: -2,
+    down: 2,
 };
 
 document.addEventListener('keydown', function (event) {
@@ -22,88 +28,77 @@ document.addEventListener('keydown', function (event) {
 });
 
 function key(event) {
-    if (event.key === '-') toggle();
-    else if (event.key === '/') window.location = '.';
-    else if (event.key === '+') window.location = 'settings';
-    if (menu) {
-        switch (event.key) {
-            case '4':
-                highlight(DIRECTION.backwards);
-                break;
-            case '5':
-                select(selection);
-                break;
-            case '6':
-                highlight(DIRECTION.forward);
-        }
-    } else {
-        switch (event.key) {
-            case '1':
-                select(6);
-                break;
-            case '2':
-                select(7);
-                break;
-            case '3':
-                select(8);
-                break;
-            case '4':
-                select(3);
-                break;
-            case '5':
-                select(4);
-                break;
-            case '6':
-                select(5);
-                break;
-            case '7':
-                select(0);
-                break;
-            case '8':
-                select(1);
-                break;
-            case '9':
-                select(2);
-                break;
-            case '0':
-                select(9);
-                break;
-            case '.':
-                select(10);
-                break;
-        }
+    switch (event.key) {
+        case '1':
+            break;
+        case '2':
+            highlight(DIRECTION.down);
+            break;
+        case '3':
+            break;
+        case '4':
+            highlight(DIRECTION.backwards);
+            break;
+        case '5':
+            select();
+            break;
+        case '6':
+            highlight(DIRECTION.forward);
+            break;
+        case '7':
+            break;
+        case '8':
+            highlight(DIRECTION.up);
+            break;
+        case '9':
+            break;
+        case '0':
+            break;
+        case '.':
+            break;
+        case '+':
+            window.location = 'settings';
+            break;
+        case '-':
+            toggle();
+            break;
+        case '/':
+            window.location = '.';
+            break;
     }
+
 }
 
 /**
  * Selects the services to navigate to
- * @param i index in list
  */
-function select(i) {
+function select() {
     if (menu) {
-        menuElements()[i].click();
+        getElements()[selection].click();
     } else {
-        const services = document.getElementById("services").childElementCount;
-        if (i < services) {
-            document.getElementsByClassName("grid-item")[i].getElementsByTagName("a")[0].click();
-        }
+        getElements()[selection].getElementsByTagName('a')[0].click();
     }
 }
 
-function menuElements() {
-    return document.getElementById("nav").getElementsByTagName("a");
+function getElements() {
+    if (menu) {
+        return document.getElementById("nav").getElementsByTagName("a");
+    } else {
+        return document.getElementsByClassName("grid-item");
+    }
 }
 
 /**
  * Toggles between the menu and the websites
  */
 function toggle() {
-    menu = !menu;
-    if (menu) {
+    if (!menu) {
+        menu = true;
         highlight(DIRECTION.none);
     } else {
         highlight(DIRECTION.remove);
         selection = 0;
+        menu = false;
     }
 }
 
@@ -112,18 +107,34 @@ function toggle() {
  * @param d direction to highlight
  */
 function highlight(d) {
-    let elements = menuElements();
+    let elements = getElements();
     if (d === DIRECTION.none) {
-        elements[selection].classList.add("selectedText");
+        if (menu) {
+            elements[selection].classList.add("selectedText");
+        } else {
+            elements[selection].classList.add("selected");
+        }
     } else if (d === DIRECTION.remove) {
-        elements[selection].classList.remove("selectedText");
-    } else if (d === DIRECTION.backwards && selection > 0) {
+        if (menu) {
+            elements[selection].classList.remove("selectedText");
+        } else {
+            elements[selection].classList.remove("selected");
+        }
+    } else if ((d === DIRECTION.backwards && selection > 0) && (menu || (!menu && selection % 3 !== 0))) {
         highlight(DIRECTION.remove);
         selection--;
         highlight(DIRECTION.none);
-    } else if (d === DIRECTION.forward && selection < elements.length - 1) {
+    } else if ((d === DIRECTION.forward && selection < elements.length - 1) && (menu || (!menu && selection % 3 !== 2))) {
         highlight(DIRECTION.remove);
         selection++;
+        highlight(DIRECTION.none);
+    } else if (d === DIRECTION.up && !menu && selection > 2) {
+        highlight(DIRECTION.remove);
+        selection -= 3;
+        highlight(DIRECTION.none);
+    } else if (d === DIRECTION.down && !menu && selection < elements.length - 3) {
+        highlight(DIRECTION.remove);
+        selection += 3;
         highlight(DIRECTION.none);
     }
 }
