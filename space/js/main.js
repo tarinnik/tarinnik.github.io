f22ab778330a1808d79c0277cdddb80285e40cdb
Tinnik <tpkoster@protonmail.com>
2022-07-04 16:51:35 +1000

    fixes

--- space/js/main.js
+++ space/js/main.js
@@ -1,16 +1,19 @@
 function main() {
-    let page = new URLSearchParams(document.location.search).get("s");
-    page = (page === null) ? "sun" : page;
+    let page = new URLSearchParams(document.location.search).get("s")
+    if (page !== null) {
+        history.replaceState({}, page, page);
+        document.title = page;
+    } else {
+        page = "sun";
+    }
     load(page);
 }
 
 function load(page) {
     const canvas = document.getElementById("canvas");
     const overview = new System(DATA[page]);
-    console.log(DATA[page].system);
     overview.addSatellites(DATA[page].system);
     overview.draw(canvas, false);
 }
 
-console.log(DATA);
 main();
