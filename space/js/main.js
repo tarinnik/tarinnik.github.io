76c6bd8dd8f5b1c5229b5a152f52fd18e7c483c5
Tinnik <tpkoster@protonmail.com>
2022-07-04 18:07:16 +1000

    capitalise title

--- space/js/main.js
+++ space/js/main.js
@@ -2,7 +2,7 @@ function main() {
     let page = new URLSearchParams(document.location.search).get("s")
     if (page !== null) {
         history.replaceState({}, page, page);
-        document.title = page;
+        document.title = capitalise(page);
     } else {
         page = "sun";
     }
@@ -14,6 +14,10 @@ function load(page) {
     const overview = new System(DATA[page]);
     overview.addSatellites(DATA[page].system);
     overview.draw(canvas, false);
+}
+
+function capitalise(word) {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
 }
 
 main();
