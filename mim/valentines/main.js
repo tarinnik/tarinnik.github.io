30384a0a7cceff6b018e578bf1a4c192db165925
Tin Nik <tinnik@protonmail.com>
2025-02-10 17:51:38 +1100

    remove

--- mim/valentines/main.js
+++
@@ -1,7 +0,0 @@
-function submit() {
-    let text = document.getElementById("text").value;
-    fetch("https://ntfy.sh/8e7f86260c88346052cadd7d68514184", {
-        method: 'POST',
-        body: text
-    }); 
-}
