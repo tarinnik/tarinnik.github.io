12b583937f03978fa032df1831ccc1ca784c1320
Tin Nik <tinnik@protonmail.com>
2025-10-29 18:16:43 +1100

    add some riddles

---
+++ hunt/riddleData.js
@@ -0,0 +1,77 @@
+const RIDDLES = [
+    {
+        "riddle": "What is the name of the punctuation mark that is a combination of a question and exclamation mark?",
+        "riddleAnswerOptions": ["Percontation point", "SarcMark", "Interrobang", "Authority point"],
+        "riddleAnswer": 2
+    },
+    {
+        "riddle": "What did Steve Jobs soak his feet in when he wanted to de-stress?",
+        "riddleAnswerOptions": ["A puddle", "A toilet", "A bucket", "A ditch"],
+        "riddleAnswer": 1
+    },
+    {
+        "riddle": "Every continent, except Antaractica, has a city called what?",
+        "riddleAnswerOptions": ["Paris", "York", "Rome (Roma)", "Seville"],
+        "riddleAnswer": 2
+    },
+    {
+        "riddle": "What colour are the hairs of a polar bear's fur?",
+        "riddleAnswerOptions": ["Cream", "Transparent", "White", "Grey"],
+        "riddleAnswer": 1
+    },
+    {
+        "riddle": "What type of a fruit is a Bing?",
+        "riddleAnswerOptions": ["Cherry", "Pear", "Plum", "Apple"],
+        "riddleAnswer": 0
+    },
+    {
+        "riddle": "What country was put up for sale on eBay in 2006?",
+        "riddleAnswerOptions": ["Malawi", "New Zealand", "Bolivia", "Malaysia"],
+        "riddleAnswer": 1
+    },
+    {
+        "riddle": "What is the plural of cul-de-sac?",
+        "riddleAnswerOptions": ["Cul-de-sac", "Cul-des-sacs", "Culs-de-sac", "Culs-des-sacs"],
+        "riddleAnswer": 2
+    },
+    {
+        "riddle": "What is the profession of both of Jack Black's parents?",
+        "riddleAnswerOptions": ["Stand-up comedian", "Nuclear physicist", "Firefighter", "Rocket scientist"],
+        "riddleAnswer": 3
+    },
+    {
+        "riddle": "Lyme disease is named after a town in wich U.S State?",
+        riddleAnswerOptions: ["Connecticut", "Idaho", "Maine", "Georgia"],
+        riddleAnswer: 0
+    },
+    {
+        riddle: "In the TV series MythBusters, a standard household vacuum cleaner lifted what?",
+        riddleAnswerOptions: ["A dog", "A bathtub", "A refrigerator", "A car"],
+        riddleAnswer: 3
+    },
+    {
+        riddle: "The legendary Man in the Iron Mask was imprisoned in which country?",
+        riddleAnswerOptions: ["Spain", "Germany", "Italy", "France"],
+        riddleAnswer: 3
+    },
+    {
+        riddle: "Which country has no mosquitoes?",
+        riddleAnswerOptions: ["Iceland", "Greenland", "Norway", "Finland"],
+        riddleAnswer: 0
+    },
+    {
+        riddle: "Which punctuation mark is used to end a sentence with a note of skepticism?",
+        riddleAnswerOptions: ["Acclamation Point", "Irony mark", "Doubt point", "Certitude point"],
+        riddleAnswer: 2
+    },
+    {
+        riddle: "As of 2018, how long was the longest recorded bout of hiccuping?",
+        riddleAnswerOptions: ["68 days", "68 years", "68 weeks", "68 hours"],
+        riddleAnswer: 1
+    },
+    {
+        riddle: "Which sea has no coastline?",
+        riddleAnswerOptions: ["Caspian Sea", "Dead Sea", "Sargasso Sea", "Ligurian Sea"],
+        riddleAnswer: 2
+    }
+];
